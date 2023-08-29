a2eee06f27c933721e4fcfdf703d7b1179c567f2
kirthi1594 <kirthikajayaraman1594@gmail.com>
2023-08-29 15:57:16 +0530

    correct it

---
+++ Git1-primeNo.js
@@ -0,0 +1,22 @@
+//Check whether a number is Prime or not
+let count = 0
+function prime(x){
+    for (let i =0; i>=x; i++){
+        if(x%i==0){
+            count++;
+        }
+    }
+        if(count==2){
+            console.log("prime")
+        }
+        else{
+            console.log("No prime")
+  
+        }
+    
+    
+}
+
+prime(1)
+
+// this is from branch code_fix
